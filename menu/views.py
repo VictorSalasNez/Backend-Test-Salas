fbf0ece67e905005606fd13f28993b98402de3c4
VictorSalasNez <victor.salasnez@gmail.com>
2021-01-31 11:47:42 -0300

    add menu basic html

--- menu/views.py
+++ menu/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-
-
 def index(request):
-    return HttpResponse("This is the menu page")
+    return render(request, 'menu/menu.html')
 
 def create(request):
-    return HttpResponse("This is were the menus/meals/salad/dessert will be created")
+    return render(request, 'menu/create.html')
