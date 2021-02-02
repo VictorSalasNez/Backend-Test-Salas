48801e03b9805cd23ccff119a4b8997767853211
VictorSalasNez <victor.salasnez@gmail.com>
2021-02-02 01:30:30 -0300

    add tables to admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Meal)
+admin.site.register(Salad)
+admin.site.register(Dessert)
+admin.site.register(Lunch)
