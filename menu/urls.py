2a88ffaf48652c35b316e259553159ec24dadf7e
VictorSalasNez <victor.salasnez@gmail.com>
2021-02-02 01:32:44 -0300

    more url id

--- menu/urls.py
+++ menu/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('create/', views.create, name='Create Page'),
+    path('', views.menu_hub, name='menu_hub'),
+    path('<uuid:menu_uuid>/', views.menus_day, name='Nora_Menu'),
+    path('create/', views.create, name='Create_Page'),
+    
 ]
