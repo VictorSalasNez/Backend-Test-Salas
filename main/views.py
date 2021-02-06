be7d6a31912e915d7df82980cf80c8884b5f216b
VictorSalasNez <victor.salasnez@gmail.com>
2021-02-06 19:44:44 -0300

    fix main page

--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from menu.models import Order, Menu
+from datetime import date
 # Create your views here.
 
 
 def dashboard(request):
-    return render(request, 'main/main.html')
+    return render(request, 'main/main.html', {'orders': Order.objects.all(), 
+                                              'today_day': date.today().day,
+                                              'menus': Menu.objects.all().order_by('day')})
