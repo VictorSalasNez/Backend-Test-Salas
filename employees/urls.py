98fb56099352c87acc539f6971ece6847184648d
VictorSalasNez <victor.salasnez@gmail.com>
2021-02-07 02:26:16 -0300

    CRUD for employee

--- employees/urls.py
+++ employees/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='employee_hub'),
+    path('', views.employee_hub, name='employee_hub'),
+    path('create/employee', views.create_employee, name='create_employee'),
+    path('update/employee/<uuid:employee_uuid>', views.update_employee, name='update_employee'),
+    path('delete/employee/<uuid:employee_uuid>', views.delete_employee, name='delete_employee'),
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Employee
+from .forms import EmployeeForm
 # Create your views here.
 
-def index(request):
-    return HttpResponse("This is the Employees page")
+def employee_hub(request):
+    return render(request, 'employees/menu.html', {'employees'  : Employee.objects.all() })
+
+
+# CREATE VIEWS
+def create_employee(request):
+    # TODO send a popup message if fail or success
+    # TODO block two menus for the same day
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = {'employeeform': EmployeeForm()}
+    return render(request, 'employees/create_employee.html', form) 
+
+
+# UPDATE VIEWS
+def update_employee(request, employee_uuid):
+    menu = Employee.objects.get(uuid=employee_uuid)
+    form = EmployeeForm(instance=menu)
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, instance=menu)
+        if form.is_valid():
+            form.save()
+            return redirect(employee_hub)
+
+    menu_form = {'employeeform': form}
+    return render(request, 'employees/create_employee.html', menu_form)
+
+# DELETE VIEWS
+def delete_employee(request, employee_uuid):
+    menu = Employee.objects.get(uuid=employee_uuid)
+    if request.method == 'POST':
+        menu.delete()
+        return redirect(employee_hub)
+
+    form = {'item': menu}
+    return render(request, "employees/delete_employee.html", form)
